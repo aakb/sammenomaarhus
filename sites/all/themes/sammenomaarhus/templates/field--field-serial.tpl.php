855458470c8e3d609b90cb75f59964d31f253ef9
Ture Gjørup <ture@ignatz.dk>
2015-04-27 17:19:38 +0200

    DELBUD-16: Layout tweaks for smartphones

--- sites/all/themes/sammenomaarhus/templates/field--field-serial.tpl.php
+++ sites/all/themes/sammenomaarhus/templates/field--field-serial.tpl.php
@@ -50,6 +50,4 @@
     <div class="field-label"<?php print $title_attributes; ?>><?php print $label ?>:&nbsp;</div>
   <?php endif; ?>
 
-    <?php foreach ($items as $delta => $item): ?>
-      <?php print render($item); ?>
-    <?php endforeach; ?>
+  <?php foreach ($items as $delta => $item): ?><?php print render($item); ?><?php endforeach; ?>
