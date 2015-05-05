945c151ebed818dab212fd185522d0ba4d9e92aa
Ture Gjørup <ture@ignatz.dk>
2015-05-05 13:40:29 +0200

    DELBUD-17: Added site improve script

--- sites/all/themes/sammenomaarhus/js/script.js
+++ sites/all/themes/sammenomaarhus/js/script.js
@@ -62,3 +62,12 @@ jQuery(document).ready(function() {
 
 
 });// END $(document).ready(function())
+
+/**
+ * SiteImprove Tracking
+ */
+(function() {
+    var sz = document.createElement('script'); sz.type = 'text/javascript'; sz.async = true;
+    sz.src = '//ssl.siteimprove.com/js/siteanalyze_2240844.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(sz, s);
+})();
